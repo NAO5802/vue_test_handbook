59f250dac6c045299ae317d160778db55816c0c8
NAO5802 <i100185b@gmail.com>
2023-11-01 21:43:57 +0900

    add component test

---
+++ src/components/Greeting.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Greeting",
+  data(){
+    return {
+      greeting: "Vue and TDD"
+    }
+  }
+})
+</script>
+
+<template>
+<div>
+  {{ greeting }}
+</div>
+</template>
+
+<style scoped>
+
+</style>
