51322236e71842b2d5579f037b418ebfcabcf4b8
NAO5802 <i100185b@gmail.com>
2023-11-10 14:34:53 +0900

    refactor props test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,52 +1,51 @@
 import {mount, shallowMount} from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
-import {Vue} from "vue-class-component";
-import Child from "@/components/Child.vue";
 import Parent from "@/components/Parent.vue";
 import SubmitButton from "@/components/SubmitButton.vue";
 
 describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+    it("renders props.msg when passed", () => {
+        const msg = "new message";
+        const wrapper = shallowMount(HelloWorld, {
+            props: {msg},
+        });
+        expect(wrapper.text()).toMatch(msg);
     });
-    expect(wrapper.text()).toMatch(msg);
-  });
 });
 
 describe("examples", () => {
 
-  it('render component', () => {
-    const shallowWrapper = shallowMount(Parent)
-    const mountWrapper = mount(Parent)
+    it('render component', () => {
+        const shallowWrapper = shallowMount(Parent)
+        const mountWrapper = mount(Parent)
 
-    console.log(shallowWrapper.html())
-    console.log(mountWrapper.html())
-  })
+        console.log(shallowWrapper.html())
+        console.log(mountWrapper.html())
+    })
 })
 
 describe('props', () => {
-  it('権限がない場合のメッセージ', () => {
-      const msg =  "送信する"
-      const wrapper = shallowMount(SubmitButton, {
-        propsData: {msg}
-      })
+    const msg = "送信する"
+    const factory = (prppsData?: any) => {
+        return shallowMount(SubmitButton, {
+            propsData: {
+                msg,
+                ...prppsData
+            }
+        })
+    }
 
-      console.log(wrapper.html())
+    it('権限がない場合のメッセージ', () => {
+        const wrapper = factory()
 
-      expect(wrapper.find('span').text()).toBe("権限がありません")
-      expect(wrapper.find('button').text()).toBe("送信する")
-  });
+        expect(wrapper.find('span').text()).toBe("権限がありません")
+        expect(wrapper.find('button').text()).toBe("送信する")
+    });
 
-  it('権限がる場合のメッセージ', () => {
-    const msg =  "送信する"
-    const isAdmin = true
-    const wrapper = shallowMount(SubmitButton, {
-      propsData: {msg, isAdmin}
-    })
+    it('権限がある場合のメッセージ', () => {
+        const wrapper = factory({isAdmin: true})
 
-    expect(wrapper.find('span').text()).toBe('管理者権限を実行する')
-    expect(wrapper.find('button').text()).toBe('送信する')
-  });
+        expect(wrapper.find('span').text()).toBe('管理者権限を実行する')
+        expect(wrapper.find('button').text()).toBe('送信する')
+    });
 })
